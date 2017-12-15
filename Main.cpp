5bcf57f59d7e09553e56704dc3e8334ec4b807e6
Franklin Garcia <franklin__garcia@unitec.edu>
2017-12-15 16:58:39 -0600

    gg

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,7 @@
 #include "Stack.h"
 #include "ArrayStack.h"
 
-void crear_archivo(string texto);
+void crear_archivo(string);
 using namespace std;
 
 
@@ -12,17 +12,18 @@ int main(){
   bool seguir=true;
   int opcion;
   while(seguir){
-    cout<<"Ingrese opcion"<<endl<<"1-Crear archivo"<<endl<<"2-Modificar un archivo"<<endl<<"3-salir";
+    cout<<"Ingrese opcion"<<endl<<"1-Crear archivo"<<endl<<"2-Modificar un archivo"<<endl<<"3-salir"<<endl;
     cin>>opcion;
     switch (opcion) {
       case 1:{
-        cout<<"Ingrese contenido"<<endl;
+        cout<<"Ingrese el contenido del archivo"<<endl;
         string contenido;
         cin>>contenido;
         crear_archivo(contenido);
+        cout<<"Hecho"<<endl;
       }break;
       case 2:{
-
+        
 
       }break;
       case 3:{
@@ -37,14 +38,33 @@ return 0;
 }
 
 void crear_archivo(string texto){//escribir texto
-        cout<<"Ingrese nombre del archivo con ruta"<<endl;
+        cout<<"Ingrese nombre y ruta del archivo"<<endl;
         string ruta;
         cin>>ruta;
+        cin>>ruta;
+        ruta+=".txt";
         ofstream archivo;
-        archivo.open(ruta,ios::out);//Abriendo o creando el archivo
+        archivo.open(ruta.c_str(),ios::out);//Abriendo o creando el archivo
         if(archivo.fail()){
                 cout<<"Hubo un error";
         }
         archivo<<texto;
+        archivo.close();
+}
+
+void lectura_archivo(){
+        ifstream archivo;//leer el archivo
+        string texto;
+        cout<<"Ingrese el nombre y ruta del archivo"<<endl;
+        string ruta;
+        cin>>ruta;
+        archivo.open(ruta.c_str(),ios::in);
+        if(archivo.fail()){
+                cout<<"Hubo un error";
+        }
+        while(!archivo.eof()){//mientras no sea el final del archivo
+                getline(archivo,texto);
+                cout<<"Mi nombre es"<<texto<<endl;
+        }
         archivo.close();
 }
--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-main: Main.cpp ArrayStack.cpp
-	g++ -o Main.cpp ArrayStack.cpp
+main: Main.o ArrayStack.o
+	g++ Main.o ArrayStack.o -o main
+
+Main.o:	Main.cpp
+	g++ -c Main.cpp
+
+ArrayStack.o:	ArrayStack.cpp
+	g++ -c ArrayStack.cpp
